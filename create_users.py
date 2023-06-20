63650dd0c37505c9f4e85868af865919299dc7a2
Anatolii Khyzun <anatoliy.fhock@gmail.com>
2023-06-20 23:24:56 +0300

    [add] create_user to test api

---
+++ create_users.py
@@ -0,0 +1,29 @@
+from werkzeug.security import generate_password_hash
+from app import db, app
+from app.models import User
+
+app.app_context().push()  # добавляем контекст приложения
+
+# Создание пользователя с ролью админа
+admin = User()
+admin.first_name = "AdminFirstName"
+admin.last_name = "AdminLastName"
+admin.email = "admin@example.com"
+admin.password = "admin_password"
+admin.is_admin = True
+admin.gender_id = 1
+db.session.add(admin)
+
+# Создание пользователя с ролью менеджера
+manager = User()
+manager.first_name = "ManagerFirstName"
+manager.last_name = "ManagerLastName"
+manager.email = "manager@example.com"
+manager.password = "manager_password"
+manager.is_manager = True
+admin.is_admin = True
+manager.gender_id = 1
+db.session.add(manager)
+
+# Сохранение изменений в базе данных
+db.session.commit()
